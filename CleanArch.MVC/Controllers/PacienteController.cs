8ecc3ddbb1166fa0c7f250728992dafac1cdecd4
Bruno Tito <brunosantostito2@hotmail.com>
2023-07-25 21:34:53 -0300

    Adicionado view Index paciente

---
+++ CleanArch.MVC/Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+﻿using Clinica.Application.Interfaces;
+using Clinica.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinica.MVC.Controllers
+{
+    public class PacienteController : Controller
+    {
+        private readonly IPacienteService _pacienteService;
+
+        public PacienteController(IPacienteService pacienteService)
+        {
+            _pacienteService = pacienteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _pacienteService.GetPacientes();
+            return View(result);
+        }
+    }
+}
